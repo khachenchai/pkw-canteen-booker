488307fc7848f4bcd04112e9a58e5cd043a6dbe3
Khachenchai <jaiklaanda@gmail.com>
2021-09-04 13:07:49 +0700

    Push

--- lib/widgets/show_image.dart
+++ lib/widgets/show_image.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ShowImage extends StatelessWidget {
   final String path;
-  const ShowImage({Key key, this.path}) : super(key: key);
+  const ShowImage({Key key, @required this.path}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
